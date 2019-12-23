2cd95ae28eaea44366bca5e001e4e2121bf77755
Suraj Wadje <suraj14wadje@gmail.com>
2019-12-23 16:04:53 +0530

    some minor changes

---
+++ project/project/src/Model/Submission.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Submission {
+	int roll_no;
+	int p_id;
+	String program;
+	String processed;
+	float plagiarism_score;
+	String text;
+	int marks;
+
+}
--- project/project/src/Model/program.java
+++ project/project/src/Model/program.java
@@ -1,8 +1,164 @@
 package Model;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.sql.Date;
+
 public class program {
 	int a_no;
 	int t_id;
+	String question;
+	String expected_output;
+	String last_date;
 	
+	Date date_of_creation;
+	
+	public program()
+	{
+		date_of_creation = new Date(System.currentTimeMillis());
+		
+	}
+	
+	public program(int a_no, int t_id, String question, String expected_output,String last_date) {
+		this.a_no = a_no;
+		this.t_id = t_id;
+		this.question = question;
+		this.expected_output = expected_output;
+		this.last_date = last_date;
+		date_of_creation = new Date(System.currentTimeMillis());
+	}
+
+	public int getA_no() {
+		return a_no;
+	}
+
+	public int getT_id() {
+		return t_id;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String getExpected_output() {
+		return expected_output;
+	}
+
+	public String getLast_date() {
+		return last_date;
+	}
+
+	public Date getDate_of_creation() {
+		return date_of_creation;
+	}
+
+	public void setA_no(int a_no) {
+		this.a_no = a_no;
+	}
+
+	public void setT_id(int t_id) {
+		this.t_id = t_id;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public void setExpected_output(String expected_output) {
+		this.expected_output = expected_output;
+	}
+
+	public void setLast_date(int d,int m,int y) {
+		last_date="";
+		last_date+= y+"-"+d+"-"+m;
+		
+	}
+
+	public void setDate_of_creation(Date date_of_creation) {
+		this.date_of_creation = date_of_creation;
+	}
+
+	public boolean createNew()
+	{
+		boolean result = false;
+		int count = 0;
+		connect connection = new connect();
+		
+
+		try 
+		{
+			
+			
+			PreparedStatement ps = connection.conn.prepareStatement("insert into program values(default,?,?,?,?,?,?);");
+			ps.setInt(1,a_no);
+			ps.setInt(2,t_id);
+			ps.setString(3,question);
+			ps.setString(4,expected_output);
+			ps.setDate(5,Date.valueOf(last_date));
+			ps.setDate(6,date_of_creation);
+			count = ps.executeUpdate();
+			ps.close();
+			connection.close();
+			if(count == 1)
+			{
+				result = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return result;
+	}
+	
+	public boolean update()
+	{
+		boolean result = false;
+		int count = 0;
+		connect connection = new connect();
+		try {
+			PreparedStatement ps = connection.conn.prepareStatement("update program set question = ?,expected_ouput=? ,last_date_of_submission=? where p_id = ?");
+			ps.setString(1,question);
+			ps.setString(2,expected_output);
+			ps.setDate(3,Date.valueOf(last_date));
+			count = ps.executeUpdate();
+			ps.close();
+			connection.close();
+			if(count == 1)
+			{
+				result = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static boolean delete(int p_id)
+	{
+		int result = 0;
+		connect connection = new connect();
+		try {
+			PreparedStatement ps = connection.conn.prepareStatement("delete from program where p_id = ? ;");
+			ps.setInt(1, p_id);
+			result = ps.executeUpdate();
+			ps.close();
+			connection.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		if(result==1)
+			return true;
+		else
+			return false;
+	}
 
 }
